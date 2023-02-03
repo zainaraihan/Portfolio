2a2860c66cb47aa2f3b96b8547f17506e8303c0d
zainarhn <zainaraihan@gmail.com>
2023-02-03 23:38:30 +0700

    website under development:

---
+++ src/components/Loading/load.jsx
@@ -0,0 +1,11 @@
+import "./load.css"
+
+const Load = () => {
+    return (
+        <div className="Load">
+            <p>Website Under Development.</p>
+        </div>
+    );
+}
+
+export default Load;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '../src/components/Loading/load'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
