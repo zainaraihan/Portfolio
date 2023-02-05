ec17eac63cfe6ee46dc2835cb7b47849377ee07a
zainarhn <zainaraihan@gmail.com>
2023-02-05 22:53:22 +0700

    Page About

--- src/components/pageAbout/index.jsx
+++ src/components/pageAbout/index.jsx
@@ -4,30 +4,41 @@ import './index.css'
 const About = () => {
     return (
         <div className="About">
-            <Nav />
-            <div className="isiAbout">
-                <div className="aboutMe">
-                    <img src="/profile.jpeg" alt="" />
-                    <div className="paragraph">
-                        <p>About <span>ME.</span></p>
-                        <p className="txt">
-                            As a talented Graphic Designer and Frontend Developer, I have a passion for combining aesthetics with functionality to bring your brand to life. With an eye for detail and a drive to exceed expectations, I work closely with clients to understand their unique needs and bring their vision to fruition. Whether it's creating a stunning logo, designing a beautiful website, or developing a custom web application, I have the skills and experience to deliver the results you're looking for. Let's work together to make your brand shine.
-                        </p>
-                    </div>
-                    <div className="CTA">
-                        <div className="behance">
-                            <a href="www.behance.net/zainarhn">BHNC -&#62;</a>
-                        </div>
-                        <div className="email">
-                            <a href="mailto:zainarhn@gmail.com">Email</a>
-                        </div>
-                        <div className="github">
-                            <a href="www.github.com/zainarhn">GTHB -&#62;</a>
+            <div className="aboutt">
+                <div className="main">
+                    <div className="isiAbout">
+                        <div className="aboutMe">
+                            <div className="img">
+                            </div>
+                            <div className="desc">
+                                <div className="paragraph">
+                                    <p className="txtAtas">About <span>ME.</span></p>
+                                    <p className="txt">
+                                        As a talented Graphic Designer and Frontend Developer, I have a passion for combining aesthetics with functionality to bring your brand to life. With an eye for detail and a drive to exceed expectations, I work closely with clients to understand their unique needs and bring their vision to fruition. Whether it's creating a stunning logo, designing a beautiful website, or developing a custom web application, I have the skills and experience to deliver the results you're looking for. Let's work together to make your brand shine.
+                                    </p>
+                                </div>
+                                <div className="CTA">
+                                    <div className="behance">
+                                        <a target={"_blank"} href="www.behance.net/zainarhn">BHNC -&#62;</a>
+                                    </div>
+                                    <div className="eemmaa">
+                                        <div className="kotak"></div>
+                                        <div className="email">
+                                            <a
+                                                target={"_blank"}
+                                                href="mailto:zainarhn@gmail.com">EMAIL</a>
+                                        </div>
+                                    </div>
+                                    <div className="github">
+                                        <a target={"_blank"} href="www.github.com/zainarhn">&#60;- GTHB</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="Skills">
+            <div className="skills">
                 <div className="isiSkills">
                     <div className="skillAtas">
                         <div className="dev">
@@ -39,37 +50,50 @@ const About = () => {
                             </div>
                         </div>
                         <div className="gd">
-                            <p>Graphic Design</p>
+                            <p>GRAPHIC DESIGN</p>
                             <div className="isiGd">
-                                <p>Logo Design</p>
-                                <p>Social Media Poster</p>
-                                <p>Motion Graphic</p>
-                                <p>Branding</p>
-                                <p>UI Design</p>
+                                <div className="satu">
+                                    <p>Logo Design</p>
+                                    <p>Social Media Poster</p>
+                                    <p>Motion Graphic</p>
+                                </div>
+                                <div className="dua">
+                                    <p>Branding</p>
+                                    <p>UI Design</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="tools">
-                        <div>
-                            <p>TOOLS</p>
+                    <div className="skillBawah">
+                        <div className="isiSkillBawah">
+                            <p className="tools">TOOLS</p>
                             <div className="isiTools">
-                                <p>Javascript</p>
-                                <p>React Js</p>
-                                <p>Vanilla CSS</p>
-                                <p>GSAP</p>
-                                <p>Node Js</p>
-                                <p>Mongo DB</p>
-                                <p>Figma</p>
-                                <p>Photoshop</p>
-                                <p>Illustrator</p>
-                                <p>After Effect</p>
-                                <p>Cinema 4D</p>
+                                <div className="satu">
+                                    <p>Javascript</p>
+                                    <p>React Js</p>
+                                    <p>Vanilla CSS</p>
+                                </div>
+                                <div className="dua">
+                                    <p>GSAP</p>
+                                    <p>Node Js</p>
+                                    <p>Mongo DB</p>
+                                </div>
+                                <div className="tiga">
+                                    <p>Figma</p>
+                                    <p>Photoshop</p>
+                                    <p>Illustrator</p>
+                                </div>
+                                <div className="empat">
+                                    <p>After Effect</p>
+                                    <p>Cinema 4D</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 }
 
