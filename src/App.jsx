24d647da635cc8a6631a6047a0387703fb4c558e
zainarhn <zainaraihan@gmail.com>
2023-07-09 13:59:24 +0700

    mengubah nama dari import load

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,6 @@ function App() {
         <Contact />
         <Footer />
       </div>
-
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../src/components/Loading/load'
+import Load from '../src/components/Loading/load'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Load />
   </React.StrictMode>,
 )
